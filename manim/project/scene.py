0dcda00bd10b1855382f69abede39e0b3a2c16ae
JamesZhang2 <jameszhang2019@outlook.com>
2022-08-09 13:04:36 -0400

    Create Intro and Rejection Sampling Scenes

--- manim/project/scene.py
+++ manim/project/scene.py
@@ -1,4 +1,115 @@
+import random
 from manim import *
+
+
+class Intro(Scene):
+    def construct(self):
+        hi = Text("Hey everyone, I'm James.")
+        justin = ImageMobject("images/Justin's Video.png")
+        hi.to_edge(UP, buff=0.2)
+        justin.scale(0.9)
+        justin.next_to(hi, DOWN)
+        self.play(Create(hi))
+        self.wait(9)
+
+        self.play(FadeIn(justin))
+        self.wait(15)
+
+        self.play(FadeOut(hi), FadeOut(justin))
+        self.wait(1)
+
+        problem = Text(
+            "How to find a uniform random point\nin a high-dimensional ball?",
+            t2c={"high-dimensional ball": RED})
+        problem.shift(UP)
+        circ_def = MathTex(
+            r"C(r) = \{(x, y) : x^2 + y^2 = r^2\}")
+        ball_def = MathTex(
+            r"B_n(r) = \{(x_1, \ldots, x_n) : x_1^2 + \ldots + x_n^2 = r^2\}")
+        circ_def.next_to(problem, DOWN)
+        ball_def.next_to(circ_def, DOWN)
+
+        self.play(Create(problem))
+        self.wait(15)
+
+        self.play(Create(circ_def))
+        self.wait(7)
+
+        self.play(Create(ball_def))
+        self.wait(10)
+
+
+class RejSampling(Scene):
+    def construct(self):
+        rej_samp_title = Text("Rejection Sampling")
+        rej_samp_title.to_edge(UP, buff=0.2)
+        self.play(Create(rej_samp_title))
+        self.wait(2)
+
+        r = 3
+        square = Square(side_length=r * 2)
+        circle = Circle(radius=r)
+
+        self.play(Create(square), Create(circle))
+        self.wait(1)
+
+        num_points = 5  # Must be greater than 1
+        points = [[0.39 * r, -0.57 * r, 0], [-0.73 * r, 0.82 * r, 0]]
+
+        for _ in range(num_points - 2):
+            points.append([2 * (random.random() - 0.5) * r,
+                           2 * (random.random() - 0.5) * r,
+                           0])
+
+        for p in points:
+            dot = Dot(point=p)
+            self.add(dot)
+
+            if (p[0] ** 2 + p[1] ** 2 <= r ** 2):
+                self.play(dot.animate.set_color(GREEN))
+            else:
+                self.play(dot.animate.set_color(RED))
+            self.wait(0.5)
+
+        self.wait(1)
+        self.play(*[FadeOut(obj) for obj in self.mobjects])
+        self.wait(1)
+
+        rs_code_raw = '''
+        def rejection_sampling(n):
+            trials = 0
+            while True:
+                trials += 1
+                vec = np.empty(n)
+                for i in range(n):
+                    vec[i] = random.random()
+                if (np.dot(vec, vec) <= 1):
+                    return (vec, trials)
+        '''
+        rs_code_rendered = Code(
+            code=rs_code_raw,
+            language="Python",
+            font="Monospace",
+            background_stroke_width=0,
+            insert_line_no=False)
+        self.play(FadeIn(rs_code_rendered))
+        self.wait(2)
+
+        trials_linear = ImageMobject(
+            "images/Trials vs. Dimension Linear.png")
+        trials_linear.scale(0.8)
+        trials_linear.align_to(ORIGIN - [0.2, 0, 0], RIGHT)
+
+        trials_log = ImageMobject(
+            "images/Trials vs. Dimension Log.png")
+        trials_log.scale(0.8)
+        trials_log.align_to(ORIGIN + [0.2, 0, 0], LEFT)
+
+        self.play(FadeOut(rs_code_rendered))
+        self.play(FadeIn(trials_linear))
+        self.wait(2)
+
+        self.play(FadeIn(trials_log))
 
 
 class BallVolume(Scene):
